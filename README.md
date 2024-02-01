5f00a3f7e972424619ba13a61aabd103b60fbfbd
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-02-01 21:26:35 +0100

    fields classification has been added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Minesweeper
+17.30
--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
+    const flagsLeft = document.querySelector('#flags-left');
     const width = 10;
+    let bombAmount = 20;
 
     console.log(grid);
     
     //create Board
     function createBoard() {
+
+        flagsLeft.innerHTML = bombAmount;
+
+        //get shuffled game array with random bombs 
+        const bombArray = Array(bombAmount).fill('bomb');
+        const emptyArray = Array(width * width - bombAmount).fill('valid');
+        const gameArray = emptyArray.concat(bombArray);
+        const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
+
+        console.log(shuffledArray);
         
         for(let i = 0; i < width * width; i++) {
             const square = document.createElement('div');
+            square.id = i;
+            square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
         }
     }
