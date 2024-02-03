a1d62a40b038ed6176c024cfd56d9f6dcd1a5609
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-02-03 20:30:49 +0100

    handling click has been added

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if (i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains("bomb")) total ++;
             if (i < 89 && squares[i + width].classList.contains("bomb")) total ++;
             squares[i].setAttribute("data", total);
-            squares[i].innerHTML = total;
+            
         }
 
         }
@@ -71,7 +71,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (square.classList.contains("bomb")) {
             gameOver();
+        }else {
+            let total = square.getAttribute("data");
+            if (total != 0) {
+                if(total === 1) square.classList.add("one");
+                if(total === 2) square.classList.add("two");
+                if(total === 3) square.classList.add("three");
+                if(total === 4) square.classList.add("four");
+                square.innerHTML = total;
+                return
+            }
+            //checkSquare(square)
         }
+
+        square.classList.add("checked");
     } 
 
     function gameOver() {
